70ce4b98a62e93dfeaed7d9ad89b5515d49c9f7e
Ken Takayama <11kenterada@gmail.com>
2020-11-16 15:59:20 +0900

    (teep_cose_test) output signed CBOR with COSE_SIGN1 if 3rd argument is specified

--- examples/teep_cose_test_main.c
+++ examples/teep_cose_test_main.c
@@ -100,5 +100,14 @@ int main(int argc, const char * argv[]) {
     print_hex(returned_payload.ptr, returned_payload.len);
     printf("\n");
 
+    if (argc > 3) {
+        size_t write_len = write_to_file(argv[3], signed_cose.len, signed_cose.ptr);
+        if (!write_len) {
+            printf("main : Can't write COSE signed CBOR file.%s,%d\n", argv[3], write_len);
+            return EXIT_FAILURE;
+        }
+        printf("\nmain : Success to write to \"%s\".\n", argv[3]);
+    }
+
     return EXIT_SUCCESS;
 }
